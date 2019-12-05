5f6a0317337822263ba4e19e11df5c195e88ac1e
Zealerr <felix.rosenberg@outlook.com>
2019-12-05 21:57:17 +0100

    second update
    small fixes with the menu style and dropdown

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,5 +1,5 @@
 var x = 0;
-
+var y = 0;
 function openMenu() {
     if (x == 0) {
         document.getElementById("mysidemenu").classList.add("sidemenuwidth");
@@ -11,4 +11,19 @@ function openMenu() {
         x = 0;
     }
 }
-  
+
+function openDropdown() {
+    if (y == 0) {
+        document.getElementById("dropdown").classList.add("open");
+        document.getElementById("dropdown").classList.remove("closed");
+        document.getElementById("fafa").setAttribute("class", "fa fa-caret-up")
+        
+        y = 1;
+    } else {
+        document.getElementById("dropdown").classList.add("closed");
+        document.getElementById("dropdown").classList.remove("open");
+        
+        document.getElementById("fafa").setAttribute("class", "fa fa-caret-down")
+        y = 0;
+    }
+}
