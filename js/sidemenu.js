9c3ed5af9ba2aaeb8aa9b01419735caa1a59e81b
Zealerr <felix.rosenberg@outlook.com>
2019-12-09 11:54:44 +0100

    Update 2
    Updating sidemenu somewhat and also content

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,5 +1,6 @@
 var x = 0;
 var y = 0;
+var z = 0;
 function openMenu() {
     if (x == 0) {
         document.getElementById("mysidemenu").classList.add("sidemenuwidth");
@@ -14,16 +15,33 @@ function openMenu() {
 
 function openDropdown() {
     if (y == 0) {
-        document.getElementById("dropdown").classList.add("open");
-        document.getElementById("dropdown").classList.remove("closed");
+        document.getElementById("dropdown1").classList.add("open");
+        document.getElementById("dropdown1").classList.remove("closed");
         document.getElementById("fafa").setAttribute("class", "fa fa-caret-up")
         
         y = 1;
     } else {
-        document.getElementById("dropdown").classList.add("closed");
-        document.getElementById("dropdown").classList.remove("open");
+        document.getElementById("dropdown1").classList.add("closed");
+        document.getElementById("dropdown1").classList.remove("open");
         
         document.getElementById("fafa").setAttribute("class", "fa fa-caret-down")
         y = 0;
+    }
+}
+
+
+function openDropdown1() {
+    if (z == 0) {
+        document.getElementById("dropdown2").classList.add("open");
+        document.getElementById("dropdown2").classList.remove("closed");
+        document.getElementById("fafa1").setAttribute("class", "fa fa-caret-up")
+        
+        z = 1;
+    } else {
+        document.getElementById("dropdown2").classList.add("closed");
+        document.getElementById("dropdown2").classList.remove("open");
+        
+        document.getElementById("fafa1").setAttribute("class", "fa fa-caret-down")
+        z = 0;
     }
 }
