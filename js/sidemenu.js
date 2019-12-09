a1b58e1ce9484ee65f25614660116d58981bbed9
Zealerr <felix.rosenberg@outlook.com>
2019-12-09 23:03:51 +0100

    sidemenu update
    remade the dropdown menu system to make it more future proof for expansion and remade the styling and animation of it. Not done but it is atleast something

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,47 +1,38 @@
 var x = 0;
-var y = 0;
-var z = 0;
 function openMenu() {
     if (x == 0) {
-        document.getElementById("mysidemenu").classList.add("sidemenuwidth");
-        document.getElementById("burgerbtn").setAttribute("class", "blueborder");
+        sideMenuOpen();
+        burgerbtnOn();
+        window.setTimeout(itemContShow, 500);
         x = 1;
     } else {
-        document.getElementById("mysidemenu").classList.remove("sidemenuwidth");
-        document.getElementById("burgerbtn").setAttribute("class", "blackborder");
+        itemContInvi();
+        window.setTimeout(sideMenuClose, 500);
+        window.setTimeout(burgerbtnOff, 500);   
         x = 0;
     }
 }
 
-function openDropdown() {
-    if (y == 0) {
-        document.getElementById("dropdown1").classList.add("open");
-        document.getElementById("dropdown1").classList.remove("closed");
-        document.getElementById("fafa").setAttribute("class", "fa fa-caret-up")
-        
-        y = 1;
-    } else {
-        document.getElementById("dropdown1").classList.add("closed");
-        document.getElementById("dropdown1").classList.remove("open");
-        
-        document.getElementById("fafa").setAttribute("class", "fa fa-caret-down")
-        y = 0;
-    }
+function sideMenuOpen() {
+    document.getElementById("sideMenu").classList.add("sidemenuwidth");
 }
 
+function sideMenuClose() {
+    document.getElementById("sideMenu").classList.remove("sidemenuwidth");
+}
 
-function openDropdown1() {
-    if (z == 0) {
-        document.getElementById("dropdown2").classList.add("open");
-        document.getElementById("dropdown2").classList.remove("closed");
-        document.getElementById("fafa1").setAttribute("class", "fa fa-caret-up")
-        
-        z = 1;
-    } else {
-        document.getElementById("dropdown2").classList.add("closed");
-        document.getElementById("dropdown2").classList.remove("open");
-        
-        document.getElementById("fafa1").setAttribute("class", "fa fa-caret-down")
-        z = 0;
-    }
+function burgerbtnOn() {
+    document.getElementById("burgerbtn").setAttribute("class", "blueborder");
+}
+
+function burgerbtnOff() {
+    document.getElementById("burgerbtn").setAttribute("class", "blackborder");
+}
+
+function itemContShow() {
+    document.getElementById("item-container").style.opacity = "1"
+}
+
+function itemContInvi() {
+    document.getElementById("item-container").style.opacity = "0"
 }
